92d50fcffce78f9d10577581936cbfc13a6c5047
Florian Louvet <florian.louvet@epitech.eu>
2020-02-01 20:16:33 +0100

    [+] Added some graphics files

---
+++ Assets/Scripts/Entity/Player/HeroController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : BaseEntity
+{
+    protected List<BaseEntity> _inRange = new List<BaseEntity>();
+    protected BaseEntity _oldTower;
+
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        
+        Move(move);
+    }
+
+    void Update()
+    {
+        int input = 0;
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+            if (CanJump())
+            {
+                AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+                AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+            }
+        }
+    }
+
+}
