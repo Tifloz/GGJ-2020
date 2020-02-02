38125100022016700b0d447853bdc37dd229be89
Tiflo <tiflozion@hotmail.fr>
2020-02-02 06:23:55 +0100

    [~] Added second player and some improvements

--- Assets/Scripts/Entity/Player/HeroController.cs
+++ Assets/Scripts/Entity/Player/HeroController.cs
@@ -8,28 +8,34 @@ public class HeroController : BaseEntity
     protected BaseEntity _oldTower;
     public bool head = true;
     private bool facingLeft = true;
+
     private void FixedUpdate()
     {
-        float move = Input.GetAxis("Horizontal");
+        float move;
+        if (head)
+            move = Input.GetAxis("Horizontal");
+        else
+            move = Input.GetAxis("Vertical");
         if (move < 0 && !facingLeft)
-            reverseImage ();
+            reverseImage();
         else if (move > 0 && facingLeft)
-            reverseImage ();
+            reverseImage();
         Move(move);
-
     }
-	void reverseImage()
-	{
-		// Switch the value of the Boolean
-		facingLeft = !facingLeft;
- 
-		// Get and store the local scale of the RigidBody2D
-		Vector2 theScale = rb.transform.localScale;
- 
-		// Flip it around the other way
-		theScale.x *= -1;
-		rb.transform.localScale = theScale;
-	}
+
+    void reverseImage()
+    {
+        // Switch the value of the Boolean
+        facingLeft = !facingLeft;
+
+        // Get and store the local scale of the RigidBody2D
+        Vector2 theScale = rb.transform.localScale;
+
+        // Flip it around the other way
+        theScale.x *= -1;
+        rb.transform.localScale = theScale;
+    }
+
     void Update()
     {
         if (Input.GetButtonDown("Jump"))
@@ -41,13 +47,19 @@ public class HeroController : BaseEntity
                 {
                     AudioSource audioSource = gameObject.GetComponent<AudioSource>();
                     AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
-                    _anim.SetBool("jump",true);
+                    StartCoroutine(WaitAndPrint());
                 }
             }
             else
             {
-                
             }
         }
+    }
+
+    IEnumerator WaitAndPrint()
+    {
+        _anim.SetBool("jump", true);
+        yield return new WaitForSeconds(2.9f);
+        _anim.SetBool("jump", false);
     }
 }
